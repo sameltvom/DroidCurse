f2fc841d035594bc5c62fb953a24edf507429bc5
Samuel Skanberg <samuel_skanberg@yahoo.se>
2010-11-20 15:28:07 +0100

    Added connect button

--- src/org/samuel/droidcurse/DroidCurse.java
+++ src/org/samuel/droidcurse/DroidCurse.java
@@ -2,6 +2,9 @@ package org.samuel.droidcurse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class DroidCurse extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,14 @@ public class DroidCurse extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ((Button)findViewById(R.id.connect_button)).setOnClickListener(connectButtonListener);
     }
+    
+    View.OnClickListener connectButtonListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Log.i("DroidCurse", "Clicked button");
+		}
+	};
 }
