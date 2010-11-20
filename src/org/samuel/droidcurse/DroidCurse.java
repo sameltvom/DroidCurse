c1e2564567793a951448540c6c7a2598d65ab501
Samuel Skanberg <samuel_skanberg@yahoo.se>
2010-11-20 15:51:09 +0100

    Added network connection class. Can now connect with a socket

--- src/org/samuel/droidcurse/DroidCurse.java
+++ src/org/samuel/droidcurse/DroidCurse.java
@@ -7,11 +7,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class DroidCurse extends Activity {
+	private NetworkConnection networkConnection;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        networkConnection = new NetworkConnection(this);
         
         ((Button)findViewById(R.id.connect_button)).setOnClickListener(connectButtonListener);
     }
@@ -20,6 +24,9 @@ public class DroidCurse extends Activity {
 		@Override
 		public void onClick(View v) {
 			Log.i("DroidCurse", "Clicked button");
+			networkConnection.setHost(networkConnection.DEFAULT_HOST);
+			networkConnection.setPort(networkConnection.DEFAULT_PORT);
+			networkConnection.connect();
 		}
 	};
 }
