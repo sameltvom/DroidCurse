6324a7fde769dc9a7fd27a6147e92209cd406788
Samuel Skanberg <samuel_skanberg@yahoo.se>
2010-11-21 23:10:34 +0100

    Added listing in artists tab as well

--- src/org/samuel/droidcurse/ArtistsActivity.java
+++ src/org/samuel/droidcurse/ArtistsActivity.java
@@ -1,15 +1,46 @@
 package org.samuel.droidcurse;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ArtistsActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+public class ArtistsActivity extends ListActivity {
+	private String[] listItems;
+	
+	private NetworkConnection networkConnection;
+	
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView textview = new TextView(this);
-        textview.setText("This is the Artists tab");
-        setContentView(textview);
+        Log.d("DroidCurse", "ArtistsActivity - onCreate");
     }
+    
+    @Override
+	protected void onStart() {
+		super.onStart();
+		Log.d("DroidCurse", "ArtistsActivity - onStart");
+		networkConnection = NetworkConnection.getInstance();
+        setContentView(R.layout.songs);
+        fillData();
+    }
+
+
+	private void fillData() {
+    	listItems = networkConnection.getListOfArtists();
+    	setListAdapter(new ArrayAdapter(this, R.layout.list_row, listItems));
+    }
+	
+	@Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        /*Intent i = new Intent(this, NoteEdit.class);
+        i.putExtra(NotesDbAdapter.KEY_ROWID, id);
+        startActivityForResult(i, ACTIVITY_EDIT);*/
+        Log.d("DroidCurse", "Clicked, position: "+position+" id: "+id);
+        //networkConnection.setArtist(position);
+        networkConnection.setArtist(position);
+    }
+
 }
