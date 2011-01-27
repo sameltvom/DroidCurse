fa936bf5213dd3b81b50d09552f355e1f2d3db5f
Samuel Skanberg <samuel_skanberg@yahoo.se>
2011-01-27 19:17:58 +0100

    Now we don't have ugly ids before artists, albums and songs. they are solely for telnet users

--- src/org/samuel/droidcurse/NetworkReaderThread.java
+++ src/org/samuel/droidcurse/NetworkReaderThread.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.LinkedList;
 
+
 import android.util.Log;
 
 public class NetworkReaderThread extends Thread {
@@ -34,9 +35,14 @@ public class NetworkReaderThread extends Thread {
 				OurLog.i("DroidCurse", "ReaderThread: Got line: "+line);
 				if (line.startsWith("ARTIST ARTIST_ITM ")) {
 					// an artist item
-					String words[] = line.split("ARTIST ARTIST_ITM ");
-					String artistItem = words[1];
-					artistMonitor.addMessage(artistItem);
+					String command[] = line.split("ARTIST ARTIST_ITM ");
+					String artistItem = command[1];
+					// We don't care about the id, maybe telnet users care,
+					// but we don't
+					String delimiter = " - ";
+					int i = artistItem.indexOf(delimiter);
+					String artist = artistItem.substring(i+delimiter.length());
+					artistMonitor.addMessage(artist);
 				} else if (line.startsWith("ARTIST ARTIST_END")) {
 					// now all artist items are sent
 					OurLog.d("DroidCurse", "ReaderThread: Getting messages from artistMonitor");
@@ -47,9 +53,13 @@ public class NetworkReaderThread extends Thread {
 				} if (line.startsWith("ALBUM ALBUM_ITM ")) {
 					OurLog.d("DroidCurse", "ReaderThread: Getting album item");
 					// an album item
-					String words[] = line.split("ALBUM ALBUM_ITM ");
-					String albumItem = words[1];
-					albumMonitor.addMessage(albumItem);
+					String command[] = line.split("ALBUM ALBUM_ITM ");
+					String albumItem = command[1];
+					// we don't care about the id
+					String delimiter = " - ";
+					int i = albumItem.indexOf(delimiter);
+					String album = albumItem.substring(i+delimiter.length());
+					albumMonitor.addMessage(album);
 				} else if (line.startsWith("ALBUM ALBUM_END")) {
 					OurLog.d("DroidCurse", "ReaderThread: Getting messages from albumMonitor");
 					LinkedList<String> listOfAlbums = albumMonitor.getMessages();
@@ -59,9 +69,13 @@ public class NetworkReaderThread extends Thread {
 					model.setAlbumList(listOfAlbums);
 				} else if (line.startsWith("LIST LIST_ITM")) {
 					// a song item
-					String words[] = line.split("LIST LIST_ITM ");
-					String songItem = words[1];
-					listMonitor.addMessage(songItem);
+					String command[] = line.split("LIST LIST_ITM ");
+					String songItem = command[1];
+					// we don't care about the id
+					String delimiter = " - ";
+					int i = songItem.indexOf(delimiter);
+					String song = songItem.substring(i+delimiter.length());
+					listMonitor.addMessage(song);
 				} else if (line.startsWith("LIST LIST_END")) {
 					OurLog.d("DroidCurse", "ReaderThread: Getting messages from listMonitor");
 					LinkedList<String> listOfSongs = listMonitor.getMessages();
@@ -71,7 +85,8 @@ public class NetworkReaderThread extends Thread {
 					OurLog.d("DroidCurse", "ReaderThread: Artists changed, fetching album list...");
 					networkConnection.sendGetListOfAlbums();
 					OurLog.d("DroidCurse", "ReaderThread: Artists changed, fetching song list...");
-					networkConnection.sendGetListOfSongs();	
+					networkConnection.sendGetListOfSongs();
+					
 				} else if (line.startsWith("INF_ALBUM")) {
 					// the artist has changed
 					OurLog.d("DroidCurse", "ReaderThread: Albums changed, fetching song list...");
